1f04b4457d9e4237885c5d9e0ee573349237fed7
ga-devfront <root@GameComputer.localdomain>
2020-04-14 19:45:35 +0200

    ✨ add routes

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,31 @@
 
 <script>
 import Nav from './components/Nav.vue';
+import Home from '.components/Home.vue';
+import About from '.components/About.vue';
+import Skills from '.components/Skills.vue';
+import Projects from '.components/Projects.vue';
+import Contact from '.components/Contact.vue';
+
 
 export default {
   name: 'App',
+  routes: {
+    '/': Home,
+    '/accueil': Home,
+    '/home': Home,
+
+    '/a-propos': About,
+    '/about': About,
+
+    '/competences': Skills,
+    '/skills': Skills,
+
+    '/projets': Projects,
+    '/projects': Projects,
+
+    '/contact': Contact,
+  },
   components: {
     Nav,
   },
