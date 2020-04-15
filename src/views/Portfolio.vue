10f316c087ae59f0a33b70718c62de784456bffc
ga-devfront <root@GameComputer.localdomain>
2020-04-15 12:00:50 +0200

    ✨ add new vue view

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+<Nav
+:currentSection="currentSection"
+:currentSectionName="currentSectionName"
+/>
+</main>
+</template>
+
+<script>
+import Nav from '../components/Nav.vue';
+
+export default {
+  name: 'Portfolio',
+  components: {
+    Nav,
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    currentSectionKey() {
+      return this.$store.state.Router.sectionMapper[window.location.pathname];
+    },
+    currentSection() {
+      return this.$store.state.Router.sections[this.currentSectionKey];
+    },
+    currentSectionName() {
+      return this.getSectionName(this.currentSection);
+    },
+  },
+  methods: {
+    initializeLang() {
+      this.$store.state.language = 'fr';
+    },
+    getSectionName(el) {
+      return el.name[this.$store.getters.lang];
+    },
+  },
+};
+</script>
+
+<style>
+body {
+    background: red;
+}
+</style>
