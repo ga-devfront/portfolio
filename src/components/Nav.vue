6017b6dff25e5c3ff07c287b745727ddde32e1ef
ga-devfront <root@GameComputer.localdomain>
2020-04-16 20:32:23 +0200

    💫 update menu animations

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,12 @@
 <template>
   <nav>
-  <span class="actual">{{$store.getters.currentSectionName}}</span>
+    <transition name="fade">
+      <span
+      :key="$store.getters.currentSectionName"
+      class="actual">
+      {{$store.getters.currentSectionName}}
+      </span>
+    </transition>
     <ul class="menu">
         <li
         v-for="section in $store.state.Router.sections"
@@ -10,7 +16,7 @@
         v-on:click.prevent.stop="$store.commit('setSection', section)"
         >
             <div class="item">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px" v-html="section.svg"></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px" v-html="section.svg"></svg>
             </div>
             <span>{{section.name[$store.getters.lang]}}</span>
         </li>
@@ -38,13 +44,20 @@ export default {
   name: 'Nav',
   methods: {
   },
+  data() {
+    return {
+      displayLang: false,
+    };
+  },
 };
 </script>
 
 <style scoped>
+/* global */
 * {
   box-sizing: border-box;
   font-family: 'Aquawax', Arial;
+  font-weight: bold;
   color: #ffffff;
 }
 ul.menu {
@@ -60,6 +73,43 @@ ul.menu {
   padding-bottom: 10px;
 }
 
+li {
+  height: 30px;
+  width: 30px;
+  margin: 4px 5px;
+  position : relative;
+  cursor: pointer;
+}
+
+li span {
+  display: inline-block;
+  opacity: 0;
+  color: white;
+  position: absolute;
+  width: max-content;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 65px;
+  transition: all 0.2s ease-in-out;
+}
+
+nav {
+  position: absolute;
+  top: 50%;
+  left: 25px;
+  transform: translateY(-50%);
+}
+
+nav>span.actual {
+  white-space:nowrap;
+  color: white;
+  position: absolute;
+  right: 50%;
+  left: 50%;
+  transform: rotate(-90deg) translate(50%);
+}
+
+/* nav buttons */
 div.item {
   display: block;
   background-color: rgba(255, 255, 255, 1);
@@ -67,49 +117,20 @@ div.item {
   height: 10px;
   width: 10px;
   border-radius: 50%;
-  margin-top: 20px;
   overflow: hidden;
-  position: relative;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   transition: all 0.2s ease-in-out;
 }
 
 li.active>div.item,
-li:not(.active) div.item:hover {
+li:not(.active):hover div.item {
   height: 30px;
   width: 30px;
-  margin-left: 5px;
-  margin-right: 5px;
   background: rgba(0, 0, 0, 0);
   border: 2px solid #fff;
-}
-
-li>button.lang {
-  margin-top: 15px;
-  border: none;
-  background: none;
-  outline: 0;
-  transition: all 0.2s ease-in-out;
-}
-
-li>button.lang:focus {
-  padding: 1px 0px 1px 0px;
-}
-
-li>button.lang ~ span {
-  display: flex;
-}
-
-li>button.lang ~ span>div {
-  width: 30px;
-}
-
-li>button.lang ~ span>div.inactive {
-  opacity: 0.5;
-  transition: all 0.2s ease-in-out;
-}
-
-li>button.lang ~ span>div.inactive:hover {
-  opacity: 1;
 }
 
 div.item>svg {
@@ -121,49 +142,66 @@ div.item>svg {
   width: 20px;
 }
 
-li {
-  position : relative;
-  cursor: pointer;
-}
-
-li>span {
-  display: inline-block;
-  opacity: 0;
-  color: white;
-  position: absolute;
-  width: max-content;
-  top: 50%;
-  left: 65px;
-  transition: all 0.2s ease-in-out;
+li>div.item ~ span {
+  pointer-events: none;
 }
 
 li.active>span {
   display: none;
 }
 
-li>div.item:hover ~ span {
+
+li:hover>div.item ~ span {
+  left: 50px;
   opacity: 1;
+}
+
+/* lang button */
+li>button.lang {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  cursor: pointer;
+  border: none;
+  background: none;
+  outline: 0;
+  transition: all 0.2s ease-in-out;
+}
+
+li>button.lang ~ span {
+  display: flex;
+  visibility: hidden;
+  transition: all 0.2s ease-in-out;
 }
 
 li>button.lang:focus ~ span {
+  visibility: visible;
+  opacity: 1;
+  left: 40px;
+}
+
+li>button.lang ~ span>div {
+  padding-left: 15px;
+}
+
+li>button.lang ~ span>div.inactive {
+  opacity: 0.5;
+  transition: all 0.2s ease-in-out;
+}
+
+li>button.lang ~ span>div.inactive:hover {
   opacity: 1;
 }
 
-nav>span.actual {
-  white-space:nowrap;
-  font-family: 'Aquawax', Arial;
-  font-weight: bold;
-  color: white;
-  position: absolute;
-  right: 50%;
-  left: 50%;
-  transform: rotate(-90deg) translate(50%);
-}
+/* class animation */
 
-nav {
-  position: absolute;
-  top: 50%;
-  left: 25px;
-  transform: translateY(-50%);
+.fade-enter-active, .fade-leave-active {
+  transition: all .2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  margin-top: -20px;
 }
 </style>
