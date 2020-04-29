7eee3062743342ab4489e9d9dfdb59d1fc49fe94
ga-devfront <root@GameComputer.localdomain>
2020-04-29 20:52:30 +0200

    ✨ add sroll suggestion

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -6,9 +6,12 @@
       <span class="title" id="firstName">Alexis</span>
     </h1>
     <transition name="fadeTxt">
-    <h2 v-bind:key="$store.state.Txt.home.title[$store.getters.lang]">{{$store.state.Txt.home.title[$store.getters.lang]}}</h2>
+    <h2 v-bind:key="$store.state.Txt.home.title[$store.getters.lang]">
+      {{$store.state.Txt.home.title[$store.getters.lang]}}
+    </h2>
     </transition>
     </div>
+    <div id="swip"><div id="circle"></div></div>
   </section>
 </template>
 
@@ -37,6 +40,7 @@ section {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 }
 
 h1 {
@@ -63,6 +67,43 @@ h2 {
   animation: enterFade 1s;
 }
 
+#titleContainer {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 800px;
+}
+
+#firstName {
+  animation: enterRight 0.8s;
+  text-align: end;
+}
+
+#secondName {
+  animation: enterLeft 0.8s;
+}
+
+#swip {
+  position: relative;
+  top: 50px;
+  height: 80px;
+  width: 30px;
+  border: 3px solid #ffffff;
+  border-radius: 25px;
+}
+
+#circle {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background: #ffffff;
+  animation: swipDown 1s linear infinite normal;
+}
+
 .title {
   height: 140px;
   line-height: 125px;
@@ -71,22 +112,36 @@ h2 {
   font-size: 6.375em;
 }
 
-#titleContainer {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  vertical-align: middle;
-  width: 800px;
+.fadeTxt-enter-active, .fadeTxt-leave-active {
+  transition: all .4s;
 }
 
-#secondName {
-  animation: enterLeft 0.8s;
+.fadeTxt-enter, .fadeTxt-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translate(-50%, 100px);
 }
 
-#firstName {
-  animation: enterRight 0.8s;
-  text-align: end;
+@keyframes swipDown {
+  0% {
+  top: 50px;
+  opacity: 0;
+  }
+  20% {
+  top: 50px;
+  opacity: 1;
+  }
+  70% {
+  top: 40px;
+  opacity: 1;
+  }
+  90% {
+  top: 10px;
+  opacity: 0;
+  }
+  100% {
+  top: 5px;
+  opacity: 0;
+  }
 }
 
 @keyframes enterLeft {
@@ -114,7 +169,7 @@ h2 {
     opacity: 0;
   }
 
-  40% {
+  20% {
     opacity: 0;
   }
 
@@ -124,47 +179,50 @@ h2 {
 }
 
 @media screen and (max-width: 820px) {
-  #titleContainer {
-    width: 550px;
-  }
-  .title {
-    margin: 0px 0px -50px 0px;
-    font-size: 3.8em;
-  }
   h2 {
     bottom: -50px;
     font-size: 1.6em;
     letter-spacing: 1px;
   }
+  #titleContainer {
+    width: 550px;
+  }
+  #swip {
+    top: 100px;
+  }
+  .title {
+    margin: 0px 0px -50px 0px;
+    font-size: 3.8em;
+  }
 }
 
 @media screen and (max-width: 500px) {
-    #titleContainer {
+  h2 {
+    bottom: -70px;
+    font-size: 1.2em;
+    letter-spacing: 1px;
+  }
+  #titleContainer {
     width: 400px;
   }
   .title {
     margin: 0px 0px -70px 0px;
     font-size: 3em;
   }
-  h2 {
-    bottom: -70px;
-    font-size: 1.2em;
-    letter-spacing: 1px;
-  }
 }
 
-@media screen and (max-width: 330px) {
-    #titleContainer {
+@media screen and (max-width: 380px) {
+  h2 {
+    bottom: -70px;
+    font-size: 1em;
+    letter-spacing: 1px;
+  }
+  #titleContainer {
     width: 310px;
   }
   .title {
     margin: 0px 0px -70px 0px;
     font-size: 2.5em;
-  }
-  h2 {
-    bottom: -70px;
-    font-size: 1em;
-    letter-spacing: 1px;
   }
 }
 </style>
