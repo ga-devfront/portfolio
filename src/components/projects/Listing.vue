80eb3b414224e979532d01e360b3c8d6f1789d87
ga-devfront <root@GameComputer.localdomain>
2020-05-14 08:42:55 +0200

    ✨ create listing of projects

---
+++ src/components/projects/Listing.vue
@@ -0,0 +1,193 @@
+<script>
+export default {
+
+  scrollEventHandler: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'scroll event handler',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  portfolio: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'portfolio',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  chaletsEtCaviar: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'chalets et caviar',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  ffpa: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'festival des films de plein air',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  expressFood: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'express food',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  botGenerator: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'super phrase generator',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  spaceShipArena: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'spaceship arena',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  foodaily: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'foodaily',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+
+  todo: {
+    preview: {
+      desktop: '',
+      mobile: '',
+    },
+    name: 'todolist',
+    logo: '',
+    type: '',
+    languages: '',
+    description: {
+      fr: '',
+      en: '',
+    },
+    links: {
+      github: '',
+      project: '',
+      doc: '',
+      npm: '',
+    },
+  },
+};
+</script>
