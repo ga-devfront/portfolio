e3f9f94410c1e114893e0f0c1ad03e528ab0a67f
ga-devfront <root@GameComputer.localdomain>
2020-05-14 12:36:47 +0200

    💄 adjust mokups

--- src/components/projects/ProjectContainer.vue
+++ src/components/projects/ProjectContainer.vue
@@ -1,8 +1,8 @@
 <template>
   <article>
-    <div>
-    <img :src="project.preview.desktop">
-    <img v-if="project.preview.mobile" :src="project.preview.mobile">
+    <div id="mokups">
+    <img id="desktop" :src="project.preview.desktop">
+    <img id="mobile" v-if="project.preview.mobile" :src="project.preview.mobile">
     </div>
     <div>
     </div>
@@ -38,5 +38,22 @@ article {
   width: 900px;
   height: 500px;
   background: aqua;
+}
+
+#mokups {
+  position: relative;
+  width: max-content;
+  top: 50%;
+  left: 0px;
+  transform: translate(0px, -50%);
+  #desktop {
+    position: relative;
+    left: 20px;
+  };
+  #mobile {
+    position: relative;
+    left: -50px;
+    bottom: -10px;
+  }
 }
 </style>
