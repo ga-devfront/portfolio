3b4d3d6e54fcea00b5bc6fc1d331f5a31062d310
ga-devfront <root@GameComputer.localdomain>
2020-05-14 12:57:52 +0200

    💫 add animation transition

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <svg
+    id="leftArrow"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -13,8 +14,11 @@
       <!--eslint-disable-next-line-->
       <path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/>
     </svg>
-    <project-container :key="currentProject.path" :project="currentProject" id="projects"/>
+    <transition :name="transition" mode="out-in">
+      <project-container :key="currentProject.path" :project="currentProject" id="projects"/>
+    </transition>
     <svg
+    id="rightArrow"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -66,6 +70,7 @@ export default {
   },
   data() {
     return {
+      transition: '',
       currentProject: Listing.project.scrollEventHandler,
     };
   },
@@ -92,9 +97,11 @@ export default {
   methods: {
     nextProject() {
       this.currentProject = Listing.project[this.nextProjectName];
+      this.transition = 'nextTransition';
     },
     prevProject() {
       this.currentProject = Listing.project[this.prevProjectName];
+      this.transition = 'prevTransition';
     },
   },
 };
@@ -117,6 +124,14 @@ section {
 }
 
 svg {
+  position: absolute;
+  z-index: 1;
+  &#leftArrow {
+    left: 12%;
+  };
+    &#rightArrow {
+    right: 12%;
+  };
   fill: $white;
   fill-opacity: 0.7;
   height: 50px;
@@ -125,5 +140,32 @@ svg {
     fill-opacity: 1;
     cursor: pointer;
   }
+}
+
+.nextTransition-enter-active,
+.nextTransition-leave-active,
+.prevTransition-enter-active,
+.prevTransition-leave-active {
+  transition: all .4s ease-in-out;
+}
+
+.nextTransition-enter  {
+  opacity: 0;
+  margin-right: -80%
+}
+
+.nextTransition-leave-to {
+  opacity: 0;
+  margin-left: -80%
+}
+
+.prevTransition-enter  {
+  opacity: 0;
+  margin-left: -80%
+}
+
+.prevTransition-leave-to {
+  opacity: 0;
+  margin-right: -80%
 }
 </style>
