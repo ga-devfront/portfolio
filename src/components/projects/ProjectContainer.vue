6aaa3bf878ea2f9bb0550173f527ee3b0a389f2d
ga-devfront <root@GameComputer.localdomain>
2020-05-14 08:42:43 +0200

    ✨ add project container

---
+++ src/components/projects/ProjectContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Project Container',
+  components: {
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
