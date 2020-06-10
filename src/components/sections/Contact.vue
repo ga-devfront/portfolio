10ef9a828cfc1a642993c8721231d4f1f9d8d3f9
ga-devfront <root@GameComputer.localdomain>
2020-06-10 08:28:57 +0200

    💫 adjusts animation

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -1,10 +1,10 @@
 <template>
   <section>
       <transition name="contact" mode="out-in">
-        <ContactForm :key="'contact' + $store.getters.lang"/>
+        <ContactForm :key="'contact' + $store.getters.lang" class="contactAnimation"/>
       </transition>
-      <transition name="social">
-        <SocialContact/>
+      <transition name="social" mode="out-in">
+        <SocialContact :key="'social' + $store.getters.lang" class="socialAnimation"/>
       </transition>
   </section>
 </template>
@@ -53,12 +53,51 @@ section {
   }
 }
 
-.contact-enter-active, .contact-leave-active {
-  transition: all .4s;
+.socialAnimation {
+  animation: socialEnter .8s ease-out;
 }
 
-.contact-enter, .contact-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.social-leave-active {
+  transition: all .4s ease-in-out;
+}
+
+.social-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translate(0px, 100%);
+}
+
+@keyframes socialEnter {
+  0% {
+    opacity: 0;
+    transform: translate(0px, 100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translate (0px, 0px);
+  }
+}
+
+.contactAnimation {
+  animation: contactEnter .8s ease-out;
+}
+
+.contact-leave-active {
+  transition: all .4s ease-in-out;
+}
+
+.contact-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
   transform: translate(0px, -100%);
+}
+
+@keyframes contactEnter {
+  0% {
+    opacity: 0;
+    transform: translate(0px, -100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translate (0px, 0px);
+  }
 }
 </style>
