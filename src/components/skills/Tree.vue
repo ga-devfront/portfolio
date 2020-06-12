8574ddf4a6f986b75c4b61851c33caaf649e4df4
ga-devfront <root@GameComputer.localdomain>
2020-06-12 10:43:10 +0200

    ✨ adding skill tree

---
+++ src/components/skills/Tree.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  developper: {
+    frontend: ['js', 'jquery', 'vuejs', 'jasmine'],
+    integration: ['html', 'css', 'bootstrap', 'sass', 'wp'],
+  },
+  tools: {
+    vcs: ['git', 'github'],
+    design: ['photoshop', 'xd', 'illustrator'],
+    environment: ['vscode', 'debian', 'npm'],
+  },
+};
+</script>
