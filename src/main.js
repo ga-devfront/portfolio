394bedcb50cf622d0da545ce8edad6972c475da6
ga-devfront <root@GameComputer.localdomain>
2020-06-19 20:00:14 +0200

    ✨ add router beforeEach for match with lang

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex';
 import VueI18n from 'vue-i18n';
 import VueRouter from 'vue-router';
 import Router from './Router.vue';
-import Txt from './Txt.vue';
 import En from './translation/En.vue';
 import Fr from './translation/Fr.vue';
 import Listing from './components/projects/Listing.vue';
@@ -19,59 +18,39 @@ const messages = {
 };
 
 const i18n = new VueI18n({
+  fallbackLocale: 'fr',
   locale: 'fr',
   messages,
 });
 
 const router = new VueRouter({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: Router.routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const { lang } = to.params;
+
+  if (!['en', 'fr'].includes(lang)) {
+    return next(i18n.locale);
+  }
+
+  if (i18n.locale !== lang) {
+    i18n.locale = lang;
+  }
+
+  return next();
 });
 
 const store = new Vuex.Store({
   state: {
-    language: null,
-    appUrl: window.location.origin,
-    currentPath: window.location.pathname,
-    Txt,
     Router,
     Listing,
   },
   mutations: {
-    setSection(state, section) {
-      const futureUrl = (typeof section.url === 'object') ? section.url[this.getters.lang] : section.url;
-      state.currentPath = futureUrl;
-      window.history.pushState(
-        {},
-        this.getters.getSectionName(section),
-        this.getters.currentUrl + state.currentPath,
-      );
-    },
-    setLang(state, lang) {
-      state.language = lang;
-    },
   },
   getters: {
-    lang: (state) => (state.language ? state.language : 'fr'),
-    getSectionName: (state, getters) => (section) => section.name[getters.lang],
-    sectionCount(state) {
-      return Object.keys(state.Router.sections).length;
-    },
-    currentPosition(state, getters) {
-      return Object.keys(state.Router.sections).indexOf(getters.currentSectionKey);
-    },
-    isActive: (state, getters) => (section) => {
-      if (section === getters.currentSection) return 'active';
-      return 'inactive';
-    },
-    isLang: (state, getters) => (lang) => {
-      if (lang === getters.lang) return 'active';
-      return 'inactive';
-    },
-    currentUrl: (state) => state.appUrl,
-    currentSectionKey: (state) => state.Router.sectionMapper[state.currentPath],
-    currentSection: (state, getters) => state.Router.sections[getters.currentSectionKey],
-    currentSectionName: (state, getters) => getters.getSectionName(getters.currentSection),
   },
 });
 
